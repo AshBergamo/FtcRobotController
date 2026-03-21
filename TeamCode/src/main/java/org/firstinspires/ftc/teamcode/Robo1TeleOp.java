6eb55b7d3d3dff0acbc694b66c1a07f7c71df4c1
Vinícius Artini Bergamo <vinicius.a.bergamo@gmail.com>
2026-03-21 08:47:18 -0300

    Update Robo1TeleOp.java
    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robo1TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robo1TeleOp.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-
 import org.firstinspires.ftc.teamcode.programas.DriveMotor;
 import org.firstinspires.ftc.teamcode.programas.IMUSensor;
 
